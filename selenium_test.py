8aff55856a1db155d00326aa40ab29dc476d1076
Aravind-pi <60608728+Aravind-pi@users.noreply.github.com>
2021-01-12 20:14:02 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -25,7 +25,7 @@ def browser():
 
 # Test step 1 - Open URL
 def test_open_url(browser):
-    browser.get("http://3.15.200.71/")
+    browser.get("http://18.220.6.22/")
 
 
 # Test step 2 - Check Title
